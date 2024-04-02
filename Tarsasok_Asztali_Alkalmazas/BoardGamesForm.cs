d6249c0f8c2696cece7a72e7b106f7724c3584f2
Bartha Ariana <ariabartha@gmail.com>
2024-04-02 17:16:05 +0200

    listing board game names from the database

--- Tarsasok_Asztali_Alkalmazas/BoardGamesForm.cs
+++ Tarsasok_Asztali_Alkalmazas/BoardGamesForm.cs
@@ -8,12 +8,35 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Configuration;
+using Tarsasok_Asztali_Alkalmazas;
+using Newtonsoft.Json;
+using System.IO;
+
 
 namespace Tarsasok_Asztali_Alkalmazas
 {
     public partial class BoardGamesForm : Form
     {
         HttpClient client = new HttpClient();
+        string endPoint = ReadSetting("endpointUrlBoardGame");
+
+        private static string ReadSetting(string keyName)
+        {
+            string result = null;
+            try
+            {
+                var value = ConfigurationManager.AppSettings;
+                result = value[keyName];
+            }
+            catch (ConfigurationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return result;
+        }
+
         public BoardGamesForm()
         {
             InitializeComponent();
@@ -21,7 +44,30 @@ namespace Tarsasok_Asztali_Alkalmazas
 
         private void BoardGamesForm_Load(object sender, EventArgs e)
         {
-            client
+            listRefreshing();
+        }
+
+        private async void listRefreshing()
+        {
+            listBoxBoardGames.Items.Clear();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(endPoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+                    var boardGame = BoardGame.FromJson(jsonString);
+                    foreach (BoardGame item in boardGame)
+                    {
+                        listBoxBoardGames.Items.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
