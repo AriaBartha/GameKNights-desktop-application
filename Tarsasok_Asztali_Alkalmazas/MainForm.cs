34179f6474ac9be4dd08ce7b30d879439ae3a48d
Bartha Ariana <ariabartha@gmail.com>
2024-04-15 22:39:37 +0200

    Comments added to MainForm

--- Tarsasok_Asztali_Alkalmazas/MainForm.cs
+++ Tarsasok_Asztali_Alkalmazas/MainForm.cs
@@ -11,13 +11,16 @@ using System.Windows.Forms;
 namespace Tarsasok_Asztali_Alkalmazas
 {
     // Az alkalmazás főoldala.
+
     public partial class MainForm : Form
     {
+        // Inicializálás.
         public MainForm()
         {
             InitializeComponent();
         }
 
+        // Board games gomb kattintási eseménye, megnyitja a BoardGamesForm ablakot.
         private void buttonToBoardGamesForm_Click(object sender, EventArgs e)
         {
             BoardGamesForm boardGamesForm = new BoardGamesForm();
@@ -25,18 +28,21 @@ namespace Tarsasok_Asztali_Alkalmazas
             //TODO: close Mainform when goes to BoardGamesForm
         }
 
+        // Employees gomb kattintási eseménye, megnyitja az EmployeeForm ablakot.
         private void buttonToEmployees_Click(object sender, EventArgs e)
         {
             EmployeeForm employeesForm = new EmployeeForm();
             employeesForm.ShowDialog();
         }
 
+        //Appointments gomb kattintási eseménye, megnyitja az AppointmentForm ablakot.
         private void buttonToAppointments_Click(object sender, EventArgs e)
         {
             AppointmentForm appointmentsForm = new AppointmentForm();
             appointmentsForm.ShowDialog();
         }
 
+        // Guests' contact data gomb kattintási eseménye, megnyitja a GuestForm ablakot.
         private void buttonToGuestscontactData_Click(object sender, EventArgs e)
         {
             GuestForm guestForm = new GuestForm();
